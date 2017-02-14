f77c91b82055b00baae297a1ea5c493b34eb0423
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-02-14 19:34:14 +0000

    ..F....... [ZBX-11365] remove extra indentation

--- frontends/php/include/views/js/configuration.action.edit.js.php
+++ frontends/php/include/views/js/configuration.action.edit.js.php
@@ -56,27 +56,27 @@
 	<div class="<?= ZBX_STYLE_TABLE_FORMS_TD_LEFT ?>"></div>
 	<div class="<?= ZBX_STYLE_TABLE_FORMS_TD_RIGHT ?>">
 		<?= (new CDiv(
-				(new CTable())
-					->addRow([
-						[
-							_('Target'),
-							(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
-							new CComboBox('opCmdTarget', null, null, [
-								'current' => _('Current host'),
-								'host' => _('Host'),
-								'hostGroup' => _('Host group')
-							]),
-							(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
-							new CVar('opCmdId', '#{opcmdid}')
-						]
+			(new CTable())
+				->addRow([
+					[
+						_('Target'),
+						(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
+						new CComboBox('opCmdTarget', null, null, [
+							'current' => _('Current host'),
+							'host' => _('Host'),
+							'hostGroup' => _('Host group')
+						]),
+						(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
+						new CVar('opCmdId', '#{opcmdid}')
+					]
+				])
+				->addRow([
+					new CHorList([
+						(new CButton('save', '#{operationName}'))->addClass(ZBX_STYLE_BTN_LINK),
+						(new CButton('cancel', _('Cancel')))->addClass(ZBX_STYLE_BTN_LINK)
 					])
-					->addRow([
-						new CHorList([
-							(new CButton('save', '#{operationName}'))->addClass(ZBX_STYLE_BTN_LINK),
-							(new CButton('cancel', _('Cancel')))->addClass(ZBX_STYLE_BTN_LINK)
-						])
-					])
-					->setAttribute('style', 'width: 100%;')
+				])
+				->setAttribute('style', 'width: 100%;')
 			))
 				->addClass(ZBX_STYLE_TABLE_FORMS_SEPARATOR)
 				->addClass(ZBX_STYLE_NOWRAP)
