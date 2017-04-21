ab4ec02846d93799adaabdd70a8d131db10aa189
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2017-04-21 09:30:19 +0000

    .......PS. [ZBX-12046] don't update cached id on all errors, not only transaction failure

--- src/libs/zbxdbcache/dbcache.c
+++ src/libs/zbxdbcache/dbcache.c
@@ -3133,14 +3133,14 @@ zbx_uint64_t	DCget_nextid(const char *table_name, int num)
 			table->recid,
 			min, max);
 
-	if (SUCCEED == DBtxn_status())
+	if (NULL != result)
 	{
+		zbx_strlcpy(id->table_name, table_name, sizeof(id->table_name));
+
 		if (NULL == (row = DBfetch(result)) || SUCCEED == DBis_null(row[0]))
 			id->lastid = min;
 		else
 			ZBX_STR2UINT64(id->lastid, row[0]);
-
-		zbx_strlcpy(id->table_name, table_name, sizeof(id->table_name));
 
 		nextid = id->lastid + 1;
 		id->lastid += num;
