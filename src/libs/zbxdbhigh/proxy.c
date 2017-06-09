47c73905689759a4d2aa45dc64dd7e19cad47d75
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2017-06-09 11:45:09 +0000

    .......PS. [DEV-611] prefix host validation function

--- src/libs/zbxdbhigh/proxy.c
+++ src/libs/zbxdbhigh/proxy.c
@@ -2593,7 +2593,7 @@ void	process_dhis_data(struct zbx_json_parse *jp)
 		zbx_json_value_by_name_dyn(&jp_row, ZBX_PROTO_TAG_VALUE, &value, &value_alloc);
 		zbx_json_value_by_name(&jp_row, ZBX_PROTO_TAG_DNS, dns, sizeof(dns));
 
-		if (FAIL == validate_hostname(dns, strlen(dns)))
+		if (FAIL == zbx_validate_hostname(dns, strlen(dns)))
 			*dns = '\0';
 
 		if (SUCCEED == zbx_json_value_by_name(&jp_row, ZBX_PROTO_TAG_STATUS, tmp, sizeof(tmp)))
@@ -2745,7 +2745,7 @@ void	process_areg_data(struct zbx_json_parse *jp, zbx_uint64_t proxy_hostid)
 		if (FAIL == zbx_json_value_by_name(&jp_row, ZBX_PROTO_TAG_DNS, dns, sizeof(dns)))
 			*dns = '\0';
 
-		if (FAIL == validate_hostname(dns, strlen(dns)))
+		if (FAIL == zbx_validate_hostname(dns, strlen(dns)))
 			*dns = '\0';
 
 		if (FAIL == zbx_json_value_by_name(&jp_row, ZBX_PROTO_TAG_PORT, tmp, sizeof(tmp)))
