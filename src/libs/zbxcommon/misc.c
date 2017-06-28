3ea2ff57f91631ac4c95ad33e613d1f4df9df6a8
Andris Mednis <Andris.Mednis@zabbix.com>
2017-06-28 12:21:03 +0000

    .......PS. [DEV-611] refactored zbx_validate_hostname()

--- src/libs/zbxcommon/misc.c
+++ src/libs/zbxcommon/misc.c
@@ -888,31 +888,45 @@ int	is_ip(const char *ip)
 	return SUCCEED == is_ip4(ip) ? SUCCEED : is_ip6(ip);
 }
 
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_validate_hostname                                            *
+ *                                                                            *
+ * Purpose: is string a valid internet hostname                               *
+ *                                                                            *
+ * Parameters: hostname - [IN] hostname string to be checked                  *
+ *                                                                            *
+ * Return value: SUCCEED - is valid hostname, FAIL - otherwise                *
+ *                                                                            *
+ ******************************************************************************/
 int	zbx_validate_hostname(const char *hostname)
 {
-	unsigned char	component = 0;	/* periods ('.') are only allowed when they serve to delimit components */
-	int		i, len;
-
-	if (0 >= (len = (int)strlen(hostname)) || 255 < len)		/* max len comes from RFC 1035 */
-		return FAIL;
+	int		component;	/* periods ('.') are only allowed when they serve to delimit components */
+	int		len = 255;	/* maximum hostname length comes from RFC 1035 (without terminating '\0') */
+	const char	*p;
 
 	/* the first character must be an alphanumeric character */
 	if (0 == isalnum(*hostname))
 		return FAIL;
 
-	/* the last character must not be a minus sign */
-	if ('-' == hostname[len - 1])
-		return FAIL;
-
-	for (i = 0; i < len; i++)
+	/* check only up to the first 'len' characters, the 1st character is already successfully checked */
+	for (p = hostname + 1, component = 1; '\0' != *p; p++)
 	{
-		if (0 != isalnum(hostname[i]) || '-' == hostname[i])
+		if (0 == --len)				/* hostname too long */
+			return FAIL;
+
+		/* check for allowed characters */
+		if (0 != isalnum(*p) || '-' == *p)
 			component = 1;
-		else if ('.' == hostname[i] && 1 == component)
+		else if ('.' == *p && 1 == component)
 			component = 0;
 		else
 			return FAIL;
 	}
+
+	/* the last character must not be a minus sign */
+	if ('-' == *(p - 1))
+		return FAIL;
 
 	return SUCCEED;
 }
