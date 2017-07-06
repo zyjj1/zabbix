d62a32171c435dd78234b8a30d3b4b99ae9c6844
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-07-06 10:26:19 +0000

    .......... [ZBXNEXT-826] updated release date [2.2.19rc1]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"19 April 2017"
+#define ZABBIX_REVDATE		"6 July 2017"
 #define ZABBIX_VERSION_MAJOR	2
 #define ZABBIX_VERSION_MINOR	2
 #define ZABBIX_VERSION_PATCH	19
