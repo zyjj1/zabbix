43d7ca46af2ca1436db25cfb6453cdeea308c39b
Miks Kronkalns <miks.kronkalns@zabbix.com>
2017-11-23 15:11:33 +0000

    ..F....... [ZBX-13024] changed regular expression to parse_url; added newly created selenium test to SeleniumTests.php

--- frontends/php/index.php
+++ frontends/php/index.php
@@ -83,10 +83,10 @@ if (isset($_REQUEST['enter']) && $_REQUEST['enter'] == _('Sign in')) {
 		$request = getRequest('request', '');
 
 		if ($request) {
-			preg_match('/^\/?(?<filename>(?:[a-z0-9\_\.]+)\.php)(?<request>\?+.*)?$/i', $request, $test_request);
+			$test_request = parse_url($request);
 
-			$request = (array_key_exists('filename', $test_request) && file_exists('./'.$test_request['filename']))
-				? $test_request['filename'].(array_key_exists('request', $test_request) ? $test_request['request'] : '')
+			$request = (array_key_exists('path', $test_request) && file_exists('./'.basename($test_request['path'])))
+				? $test_request['path'].(array_key_exists('query', $test_request) ? '?'.$test_request['query'] : '')
 				: '';
 		}
 
