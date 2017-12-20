296a7807d60d98061428b0403208b8934cab0169
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2017-12-20 10:50:37 +0000

    ..F....... [ZBX-13078] fixed javascript errors related to checkbox range select event handler

--- frontends/php/popup_right.php
+++ frontends/php/popup_right.php
@@ -59,14 +59,16 @@ $hostGroups = API::HostGroup()->get([
 
 order_result($hostGroups, 'name');
 
+$row_index = 1;
 foreach ($hostGroups as $hostGroup) {
 	$hostGroupTable->addRow([
-		(new CCheckBox())
+		(new CCheckBox('groups['.$row_index.']', $hostGroup['groupid']))
 			->setAttribute('data-id', $hostGroup['groupid'])
 			->setAttribute('data-name', $hostGroup['name'])
 			->setAttribute('data-permission', $permission),
 		$hostGroup['name']
 	]);
+	$row_index++;
 }
 
 $hostGroupTable->setFooter(
