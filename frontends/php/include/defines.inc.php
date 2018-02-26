1a20f105b6249c454a22f445da858a25a706726b
Natalja Karpinska <natalja.karpinska@zabbix.com>
2018-02-26 14:16:20 +0000

    .......... [ZBXNEXT-826] updated release version [3.0.16rc1]; updated API version [3.0.16]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'3.0.15');
-define('ZABBIX_API_VERSION',	'3.0.15');
+define('ZABBIX_VERSION',		'3.0.16rc1');
+define('ZABBIX_API_VERSION',	'3.0.16');
 define('ZABBIX_EXPORT_VERSION',	'3.0');
 define('ZABBIX_DB_VERSION',		3000000);
 
