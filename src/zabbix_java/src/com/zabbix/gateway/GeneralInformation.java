25fac81556ca1e1307bb5cc8ad19ec70a33e7553
Natalja Karpinska <natalja.karpinska@zabbix.com>
2018-05-09 12:12:03 +0000

    .......... [ZBXNEXT-826] updated release version [3.0.18rc1]; updated API version [3.0.18]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "9 May 2018";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "3.0.17";
+	public static final String VERSION = "3.0.18rc1";
 
 	public static void printVersion()
 	{
