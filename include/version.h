d8e19adb4e42cb8b20aadc048f3e47c0cd37c5bf
Natalja Karpinska <natalja.karpinska@zabbix.com>
2018-06-18 08:07:47 +0000

    .......... [ZBXNEXT-826] updated release date [3.0.19rc1]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"4 June 2018"
+#define ZABBIX_REVDATE		"18 June 2018"
 #define ZABBIX_VERSION_MAJOR	3
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	19
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "4 June 2018";
+	public static final String REVISION_DATE = "18 June 2018";
 	public static final String REVISION = "{ZABBIX_REVISION}";
 	public static final String VERSION = "3.0.19rc1";
 
