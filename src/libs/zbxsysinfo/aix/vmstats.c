79f61fe533ef17e8737768e474938e0ec7477fc4
Andris Mednis <Andris.Mednis@zabbix.com>
2018-09-04 13:59:46 +0000

    ...G...... [ZBX-14779] fixed support of 'system.stat[ent]' on AIX regardless of LPAR type (shared or dedicated)

--- src/libs/zbxsysinfo/aix/vmstats.c
+++ src/libs/zbxsysinfo/aix/vmstats.c
@@ -70,18 +70,13 @@ int	SYSTEM_STAT(AGENT_REQUEST *request, AGENT_RESULT *result)
 
 	if (0 == strcmp(section, "ent"))
 	{
-		if (1 != request->nparam && 0 != collector->vmstat.shared_enabled)
+		if (1 != request->nparam)
 		{
 			SET_MSG_RESULT(result, zbx_strdup(NULL, "Invalid number of parameters."));
 			return SYSINFO_RET_FAIL;
 		}
-		else if (0 == collector->vmstat.shared_enabled)
-		{
-			SET_MSG_RESULT(result, zbx_strdup(NULL, "No data available in collector."));
-			return SYSINFO_RET_FAIL;
-		}
-		else
-			SET_DBL_RESULT(result, collector->vmstat.ent);
+
+		SET_DBL_RESULT(result, collector->vmstat.ent);
 	}
 	else if (NULL == type)
 	{
