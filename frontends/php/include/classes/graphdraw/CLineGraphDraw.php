2e7168df6d4e0c6175780e8e7648a5d32c2ac9a8
Miks Kronkalns <miks.kronkalns@zabbix.com>
2018-09-25 14:33:10 +0000

    ..F....... [ZBX-14663] fixed calculation of Y zero position in graph

--- frontends/php/include/classes/graphdraw/CLineGraphDraw.php
+++ frontends/php/include/classes/graphdraw/CLineGraphDraw.php
@@ -757,7 +757,7 @@ class CLineGraphDraw extends CGraphDraw {
 				}
 			}
 			else {
-				$this->zero[$side] = $this->sizeY + $this->shiftY - (int) abs($this->m_minY[$side] / $this->unit2px[$side]);
+				$this->zero[$side] = $this->sizeY + $this->shiftY - (float) abs($this->m_minY[$side] / $this->unit2px[$side]);
 				$this->oxy[$side] = 0;
 			}
 		}
