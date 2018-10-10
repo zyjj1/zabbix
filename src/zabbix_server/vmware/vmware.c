fd654d5fc564f54f9a6aa9248fee542f2d326a28
Michael Veksler <Mihails.Vekslers@zabbix.com>
2018-10-10 14:48:46 +0000

    .......PS. [ZBX-14972] fix of zbx_vector_ptr_search() for vmware_service_remove()

--- src/zabbix_server/vmware/vmware.c
+++ src/zabbix_server/vmware/vmware.c
@@ -4523,7 +4523,7 @@ static void	vmware_service_remove(zbx_vmware_service_t *service)
 
 	zbx_vmware_lock();
 
-	if (FAIL != (index = zbx_vector_ptr_search(&vmware->services, &service, ZBX_DEFAULT_PTR_COMPARE_FUNC)))
+	if (FAIL != (index = zbx_vector_ptr_search(&vmware->services, service, ZBX_DEFAULT_PTR_COMPARE_FUNC)))
 	{
 		zbx_vector_ptr_remove(&vmware->services, index);
 		vmware_service_shared_free(service);
