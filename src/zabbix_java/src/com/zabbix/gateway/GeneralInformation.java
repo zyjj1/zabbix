3c378088d411b76a7e8f150a2758d2209ddf8226
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2019-01-22 08:29:03 +0000

    .......... [ZBXNEXT-826] updated release date [3.0.25rc1]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "26 November 2018";
+	public static final String REVISION_DATE = "22 January 2019";
 	public static final String REVISION = "{ZABBIX_REVISION}";
 	public static final String VERSION = "3.0.25rc1";
 
