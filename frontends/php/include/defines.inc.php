981b7c35afb845a5a93de4230972f93a7a56a0f9
Natalja Karpinska <natalja.karpinska@zabbix.com>
2019-04-23 11:12:27 +0300

    .......... [ZBXNEXT-826] updated release version [3.0.28rc1]; updated API version [3.0.28]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'3.0.27');
-define('ZABBIX_API_VERSION',	'3.0.27');
+define('ZABBIX_VERSION',		'3.0.28rc1');
+define('ZABBIX_API_VERSION',	'3.0.28');
 define('ZABBIX_EXPORT_VERSION',	'3.0');
 define('ZABBIX_DB_VERSION',		3000000);
 
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "17 April 2019";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "3.0.27";
+	public static final String VERSION = "3.0.28rc1";
 
 	public static void printVersion()
 	{
