e71ad2193baf68866b97e830733228735f889144
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2019-05-28 15:25:39 +0300

    .......... [ZBXNEXT-826] updated release version [3.0.29rc1]; updated API version [3.0.29]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'3.0.28');
-define('ZABBIX_API_VERSION',	'3.0.28');
+define('ZABBIX_VERSION',		'3.0.29rc1');
+define('ZABBIX_API_VERSION',	'3.0.29');
 define('ZABBIX_EXPORT_VERSION',	'3.0');
 define('ZABBIX_DB_VERSION',		3000000);
 
