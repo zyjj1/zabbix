247e6e36e773ef2cade749550cea095134fbdf77
Roberts Lataria <roberts.lataria@zabbix.com>
2019-11-20 17:57:16 +0200

    ..F....... [ZBX-16919] fixed server check warning element width in chrome

--- frontends/php/js/servercheck.js
+++ frontends/php/js/servercheck.js
@@ -27,15 +27,18 @@ jQuery(function($) {
 	 * @type {Object}
 	 */
 	var checker = {
+		elem: document.querySelector('.msg-global-footer'),
+		delay: 5000, // 5 seconds
 		timeout: 10000, // 10 seconds
-		warning: false,
+		is_show: false,
+		last_response: null,
 
 		/**
 		 * Sends ajax request to get Zabbix server availability and message to show if server is not available.
 		 *
-		 * @param nocache add 'nocache' parameter to get result not from cache
+		 * @param {boolean} nocache  Add 'nocache' parameter to get result not from cache.
 		 */
-		check: function(nocache) {
+		call: function(nocache) {
 			var params = nocache ? {nocache: true} : {};
 			new RPC.Call({
 				'method': 'zabbix.status',
@@ -44,64 +47,131 @@ jQuery(function($) {
 			});
 		},
 
-		onSuccess: function(result) {
-			if (result.result) {
-				this.hideWarning();
+		/**
+		 * Parse ajax responses and show / hide warning message.
+		 *
+		 * @param {object} response  Ajax response.
+		 */
+		onSuccess: function(response) {
+			if (response.result === this.last_response) {
+				return false;
 			}
-			else {
-				this.showWarning(result.message);
+
+			this.last_response = response.result;
+
+			if (response.result) {
+				return this.hide();
 			}
+
+			this.setMessage(response.message);
+			return this.show();
 		},
 
-		showWarning: function(message) {
-			if (!this.warning) {
-				$('#msg-global-footer').text(message);
-				$('#msg-global-footer').fadeIn(100);
-				this.warning = true;
-			}
+		/**
+		 * Start server status checks with 5 sec delay after page is loaded.
+		 */
+		init: function() {
+			return window.setTimeout(function () {
+				// Looping function that check for server status every 10 seconds.
+				return window.setInterval(function() {
+					checker.call(true);
+				}, checker.timeout);
+			}, this.delay);
 		},
 
-		hideWarning: function() {
-			if (this.warning) {
-				$('#msg-global-footer').fadeOut(100);
-				this.warning = false;
+		/**
+		 * Set warning message.
+		 *
+		 * @param {string} message  Warning message.
+		 */
+		setMessage: function(message) {
+			this.elem.innerText = message;
+		},
+
+		/**
+		 * Show warning message.
+		 */
+		show: function() {
+			if (this.is_show || this.last_response) {
+				return false;
 			}
+
+			this.is_show = true;
+
+			var value_opacity = 0;
+
+			this.elem.style.opacity = value_opacity;
+			this.elem.style.display = 'flex';
+
+			(function anim() {
+				if (!checker.is_show) {
+					return false;
+				}
+
+				checker.elem.style.opacity = (value_opacity += .1);
+				if (checker.elem.style.opacity < 1) {
+					requestAnimationFrame(anim);
+				}
+				else {
+					checker.elem.style.removeProperty('opacity');
+				}
+			})();
+		},
+
+		/**
+		 * Hide warning message.
+		 */
+		hide: function() {
+			if (!this.is_show) {
+				return false;
+			}
+
+			this.is_show = false;
+
+			var value_opacity = 1;
+
+			this.elem.style.opacity = value_opacity;
+			this.elem.style.display = 'flex';
+
+			(function anim() {
+				if (checker.is_show) {
+					return false;
+				}
+
+				checker.elem.style.opacity = (value_opacity -= .1);
+				if (checker.elem.style.opacity < 0.1) {
+					checker.elem.style.display = 'none';
+					checker.elem.style.removeProperty('opacity');
+				}
+				else {
+					requestAnimationFrame(anim);
+				}
+			})();
 		}
 	};
 
-	// looping function that check for server status every 10 seconds
-	function checkStatus(nocache) {
-		checker.check(nocache);
+	checker.init();
 
-		window.setTimeout(checkStatus, checker.timeout);
-	}
-
-	// start server status checks with 5 sec dealy after page is loaded
-	window.setTimeout(function() {
-		checkStatus(true);
-	}, 5000);
-
-
-	// event that hide warning message when mouse hover it
-	$('#msg-global-footer').on('mouseenter', function() {
-		var obj = $(this),
-			offset = obj.offset(),
+	// Event that hide warning message when mouse hover it.
+	$(checker.elem).on('mouseenter', function() {
+		var $obj = $(this),
+			offset = $obj.offset(),
 			x1 = Math.floor(offset.left),
-			x2 = x1 + obj.outerWidth(),
+			x2 = x1 + $obj.outerWidth(),
 			y1 = Math.floor(offset.top),
-			y2 = y1 + obj.outerHeight();
+			y2 = y1 + $obj.outerHeight();
 
-		obj.fadeOut(100);
+		checker.hide();
 
 		$(document).on('mousemove.messagehide', function(e) {
 			if (e.pageX < x1 || e.pageX > x2 || e.pageY < y1 || e.pageY > y2) {
-				obj.fadeIn(100);
+				checker.show();
 				$(document).off('mousemove.messagehide');
 				$(document).off('mouseleave.messagehide');
 			}
 		});
 		$(document).on('mouseleave.messagehide', function() {
-			obj.fadeIn(100);
+			checker.show();
 			$(document).off('mouseleave.messagehide');
 			$(document).off('mousemove.messagehide');
 		});
