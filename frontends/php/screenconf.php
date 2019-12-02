15910c4cd6460e3c5caa00fd80ea00b85230087e
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2019-12-02 14:02:00 +0200

    ..F....... [ZBX-16111] removed unnecessary expression item

--- frontends/php/screenconf.php
+++ frontends/php/screenconf.php
@@ -291,7 +291,7 @@ if (hasRequest('form')) {
 	$userids[$current_userid] = true;
 	$user_groupids = [];
 
-	if (!hasRequest('templateid') && (!array_key_exists('templateid', $screen) || !$screen['templateid'])) {
+	if (!hasRequest('templateid') && !array_key_exists('templateid', $screen)) {
 		if (!hasRequest('screenid') || hasRequest('form_refresh')) {
 			// Screen owner.
 			$screen_owner = getRequest('userid', $current_userid);
