16e187f4024f1d85731a7c5b09be8ef953db861f
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2020-03-23 18:53:06 +0200

    .......... [ZBXNEXT-826] updated release version [4.0.20rc1]; updated API version [4.0.20]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.19');
-define('ZABBIX_API_VERSION',	'4.0.19');
+define('ZABBIX_VERSION',		'4.0.20rc1');
+define('ZABBIX_API_VERSION',	'4.0.20');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
 
