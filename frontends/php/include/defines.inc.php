de32d35dbd2931a35f72c99a5cb37238a04451e0
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2020-04-19 22:37:37 +0300

    .......... [ZBX-17390] added a comment

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -37,7 +37,8 @@ define('ZBX_MEBIBYTE',	'1048576');
 define('ZBX_GIBIBYTE',	'1073741824');
 
 define('ZBX_MIN_PERIOD',		60); // 1 minute
-// the maximum period for the time bar control, ~2 years (2 * 365 * 86400) + 86400 + 3600 + 1
+// The maximum period for the time bar control, ~2 years (2 * 365 * 86400) + 86400 + 3600 + 1.
+// Both dates are included to the period, so one second is needed to cover the case from "now" till "now-2y".
 define('ZBX_MAX_PERIOD',		63162001);
 define('ZBX_MIN_INT32',			-2147483648);
 define('ZBX_MAX_INT32',			2147483647);
