03ea413601d4f54651639550e8dbf1e86d253284
Natalja Karpinska <natalja.karpinska@zabbix.com>
2020-05-27 15:18:34 +0300

    .......... [DEV-1369] added new Selenium test for widgets with option Dynamic item

--- frontends/php/tests/selenium/SeleniumTests.php
+++ frontends/php/tests/selenium/SeleniumTests.php
@@ -66,6 +66,7 @@ require_once dirname(__FILE__).'/testPageStatusOfZabbix.php';
 require_once dirname(__FILE__).'/testPageTemplates.php';
 require_once dirname(__FILE__).'/testPageUserGroups.php';
 require_once dirname(__FILE__).'/testPageUsers.php';
+require_once dirname(__FILE__).'/dashboard/testDynamicItemWidgets.php';
 require_once dirname(__FILE__).'/dashboard/testProblemsBySeverityWidget.php';
 require_once dirname(__FILE__).'/testFormAction.php';
 require_once dirname(__FILE__).'/testFormAdministrationAuthenticationHttp.php';
@@ -188,6 +189,7 @@ class SeleniumTests {
 		$suite->addTestSuite('testPageUserGroups');
 		$suite->addTestSuite('testPageUsers');
 		$suite->addTestSuite('testPageWeb');
+		$suite->addTestSuite('testDynamicItemWidgets');
 		$suite->addTestSuite('testProblemsBySeverityWidget');
 		$suite->addTestSuite('testFormAction');
 		$suite->addTestSuite('testFormAdministrationAuthenticationHttp');
