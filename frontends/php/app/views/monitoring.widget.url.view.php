e1eada40ac64f4cc231932d64966a710645aea09
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-06-12 17:53:27 +0300

    ..F....... [DEV-1566] fixed coding style

--- frontends/php/app/views/monitoring.widget.url.view.php
+++ frontends/php/app/views/monitoring.widget.url.view.php
@@ -18,6 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 **/
 
+
 if ($data['url']['error'] !== null) {
 	$item = (new CTableInfo())->setNoDataMessage($data['url']['error']);
 }
