582a2a42c0cc86958fffceb3043c1d9d79dd7191
Roberts Lataria <roberts.lataria@zabbix.com>
2020-07-30 17:32:29 +0300

    ..F....... [ZBX-18160] fixed fatal error in items.php and host_discovery.php

--- frontends/php/items.php
+++ frontends/php/items.php
@@ -1286,7 +1286,12 @@ elseif (hasRequest('action') && getRequest('action') === 'item.massclearhistory'
 
 	if ($items) {
 		// Check items belong only to hosts.
-		$hosts_status = array_column(array_column(array_column($items, 'hosts'), 0), 'status');
+		$hosts_status = [];
+		foreach (zbx_objectValues($items, 'hosts') as $value) {
+			// Item can contain only one host.
+			$hosts_status[] = $value[0]['status'];
+		}
+
 		if (in_array(HOST_STATUS_TEMPLATE, $hosts_status)) {
 			$result = false;
 		}
@@ -2022,8 +2027,12 @@ else {
 
 	// Set is_template false, when one of hosts is not template.
 	if ($data['items']) {
-		$hosts_status = array_column(array_column(array_column($data['items'], 'hosts'), 0), 'status');
-		foreach ($hosts_status as $value) {
+		$hosts_status = [];
+		foreach (zbx_objectValues($data['items'], 'hosts') as $value) {
+			// Item can contain only one host.
+			$hosts_status[] = $value[0]['status'];
+		}
+		foreach (array_unique($hosts_status) as $value) {
 			if ($value != HOST_STATUS_TEMPLATE) {
 				$data['is_template'] = false;
 				break;
